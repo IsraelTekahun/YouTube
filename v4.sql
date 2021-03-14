ec9e466e91bf1f107e31f09b7a9ad9fc30abe925
kiti17 <hakiam00@gmail.com>
2021-03-14 19:28:11 +0100

    v 4

--- v4.sql
+++ v4.sql
@@ -7,8 +7,8 @@ CREATE TABLE If not EXISTS Books (
   Period INT,
   PRIMARY KEY(Name)
 );
-
-/*INSERT INTO Books VALUES ('bk1', 'novel', 'au1', 535, 45, 2);
+/*
+INSERT INTO Books VALUES ('bk1', 'novel', 'au1', 535, 45, 2);
 INSERT INTO Books VALUES ('bk2', 'fiction', 'au3', 154, 3, 1);
 INSERT INTO Books (Name, Author, Pages, Quantity)  VALUES ('bk3','au1', 112, 104);
 INSERT INTO Books VALUES ('bk4', 'comic', 'au5', 43, 199, 4);
@@ -19,7 +19,7 @@ INSERT INTO Books (Name, Author, Pages, Quantity, Period) VALUES ('bk8', 'au2', 
 INSERT INTO Books VALUES ('bk9', 'detective', 'au1', 325, 34, 2);
 INSERT INTO Books VALUES ('bk10', 'novel', 'au4', 642, 55, 2);*/
 
--- select * from Books;
+select * from Books;
 
 -- default values  ... unknown to category and 1 to period
 	-- syntax: column name data type default value,
@@ -31,13 +31,17 @@ INSERT INTO Books VALUES ('bk10', 'novel', 'au4', 642, 55, 2);*/
 -- select * from Books limit 2;
 -- ***************************************************************************** --
 
--- where
+-- where with int, null, and strings
 	-- syntax: sql query where condition;
 /*
 select * from Books;
 select * from Books where Category = 'novel';
 select * from Books where Author = 'au1';
-select * from Books where Period = 3;*/
+select * from Books where Period = 3;
+select * from Books where Category is NULL;*/
+
+-- ***************************************************************************** --
+
 -- limit and where with int, null and strings
 -- select Name from Books where Period = 2 limit 2;
 
